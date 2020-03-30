519e72c195439b265e18525c02375aa17bcbcd56
Sarah Vivoli <snvivoli@gmail.com>
2020-03-30 13:57:27 -0700

    mongoose connection

---
+++ client/build/precache-manifest.81d4369bc0557bb2ba08ad6751ccafe9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10a33b7da936f6586260fbc6e134eca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a30aede98e9f562ccdfa",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0a9053021834c04f903c",
+    "url": "/static/js/2.cef3c251.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cef3c251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a30aede98e9f562ccdfa",
+    "url": "/static/js/main.d203baa2.chunk.js"
+  },
+  {
+    "revision": "642a50139b1ee8174e1c",
+    "url": "/static/js/runtime-main.d8327dc9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -18,6 +19,8 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
