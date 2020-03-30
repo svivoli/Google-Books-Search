f1d1a72c28843b1f1d597f5c55d40b2daa3ea5fe
Sarah Vivoli <snvivoli@gmail.com>
2020-03-30 14:32:07 -0700

    jumbotron skeleton

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Google Books</h1>
+                <p className="lead">Search for and save books from Google.</p>
+            </div>
+        </div>
+    )
+};
+
+export default Jumbotron;
