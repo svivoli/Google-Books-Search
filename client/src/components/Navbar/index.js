a859e9427864f3581630865d6e161f4156b4a5d0
Sarah Vivoli <snvivoli@gmail.com>
2020-03-30 14:28:34 -0700

    navbar links aligned properly

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,17 +1,19 @@
-import React from "./node_modules/react";
+import React from "react";
 
 function Navbar() {
     return (
-        <nav className="navbar">
+        <nav className="navbar navbar-expand-lg">
             <a class="navbar-brand" href="/">Google Books</a>
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <a class="nav-link" href="/">Search</a>
-                </li>
-                <li className="nav-item">
-                    <a class="nav-link" href="/">Saved</a>
-                </li>
-            </ul>
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a class="nav-link" href="/">Search</a>
+                    </li>
+                    <li className="nav-item">
+                        <a class="nav-link" href="/">Saved</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 };
