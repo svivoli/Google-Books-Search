363bb354ffb8460086853b72318b287f303c762f
Sarah Vivoli <snvivoli@gmail.com>
2020-03-30 14:16:25 -0700

    navbar skeleton

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React from "./node_modules/react";
+
+function Navbar() {
+    return (
+        <div>
+            <h1>Google Books</h1>
+            <a href="/">Search</a>
+            <a href="/saved">Saved</a>
+        </div>
+    )
+};
+
+export default Navbar;
