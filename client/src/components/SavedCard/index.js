7778220b4802fa61155303971585660c7844581c
Sarah Vivoli <snvivoli@gmail.com>
2020-03-31 12:11:19 -0700

    saved card skeleton

---
+++ client/src/components/SavedCard/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function SavedCard(props) {
+    return (
+        <div className="card container">
+            <div class="row">
+                <div class="col-md-4 text-center">
+                    <h3>{props.title}</h3>
+                    <img src={props.thumbnail} alt="Cover"></img>
+                </div>
+                <div className="col-md-8">
+                    <div className="col-md-3 align-self-end">
+                        <button className="btn btn-danger">Delete</button>
+                    </div>
+                    <p>{props.description}</p>
+                    <a href={props.link} target="_blank">Preview</a>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default SavedCard;
