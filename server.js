861800c1598d40cd4d77c0e00553357531bf22fe
Sarah Vivoli <snvivoli@gmail.com>
2020-03-31 11:56:19 -0700

    removed client/build/index.html route from server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
+const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const routes = require('./routes');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -15,11 +14,6 @@ if (process.env.NODE_ENV === "production") {
 
 // Define API routes here
 app.use(routes);
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
 
